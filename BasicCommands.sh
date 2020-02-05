2e6d1c4a45ec6859c8ab45f9af47df0ab385585c
Sanjay S <18615926+SanjaySasimandir@users.noreply.github.com>
2020-02-05 11:44:44 +0530

    Create BasicCommands.sh

---
+++ BasicCommands.sh
@@ -0,0 +1,23 @@
+echo Current Directory is $PWD
+echo
+
+echo Currently Logged in Users are $LOGNAME
+echo
+
+echo Shell is $SHELL
+echo
+
+echo Current Directory is $HOME
+echo
+
+echo OS Type is $OSTYPE
+echo 
+
+echo System Architecture is $(arch)
+echo
+
+echo Currently Logged in number of users is $USER
+echo
+
+echo Current Path Setting is $PATH
+echo
